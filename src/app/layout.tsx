c0c2122b7e92a5a579244a56bef9057214aecf31
Your Name <marckmorris@gmail.com>
2025-09-11 20:58:46 +0000

    el fondo se mantiene en celeste, el fondo de las tablas en blanco por fa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} dark`}>
+    <html lang="en" className={`${inter.variable}`}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
